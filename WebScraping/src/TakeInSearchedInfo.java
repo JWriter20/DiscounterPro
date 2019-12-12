79d42713470dec7c5ad06909ac540c4d70ecd651
Jake Writer <jakewriter@Jakes-MacBook-Pro.local>
2019-12-11 22:37:49 -0700

    worked on java and HTML elements

--- WebScraping/src/TakeInSearchedInfo.java
+++ WebScraping/src/TakeInSearchedInfo.java
@@ -5,15 +5,21 @@ import org.jsoup.select.Elements;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TakeInSearchedInfo {
     public static void main(String[] args) throws IOException {
         String results = "{itemList:[";
+        Scanner s = new Scanner(System.in);
+        System.out.println("Type in what you want to search for: ");
+        String itemToSearch = s.nextLine();
         ArrayList<String[]>allDataValues = new ArrayList<String[]>();
         int i =1;
         int cancelFactor =0;
-        while(cancelFactor <2){
-            String url = "https://www.amazon.com/s?k=tennis&" + "page=" + i + "&qid=1572373321&ref=sr_pg_" + i + "";
+        String lastPageNum = "Next→";
+        while(lastPageNum.equals("Next→")){
+            String url = "https://www.amazon.com/s?k=" + itemToSearch + "&page=" + i + "&qid=1572373321&ref=sr_pg_" + i + "";
+            System.out.println(url);
             Document page = Jsoup.connect(url).userAgent("Jsoup searchedInfo").get();
             String boxSelector = "div.sg-row";
             String titleSelector = "h2.a-size-mini";
@@ -21,35 +27,38 @@ public class TakeInSearchedInfo {
             String currPriceSelector = "span.a-price";
             String starSelector = "span.a-declarative";
             String oldPriceSelector = "span.a-price.a-text-price";
+            String lastPage = "li.a-last";
 
+            lastPageNum = (page.select(lastPage).text());
             boolean wasPrinted = false;
             Elements boxSelectorElements = page.select(boxSelector);
             for (int j = 3; j < boxSelectorElements.size(); j++) {
                 if (!(boxSelectorElements.get(j).select(oldPriceSelector).text().equals(""))) {
                     String picture = boxSelectorElements.get(j).select(pictureSelector).attr("src");
                     String title = boxSelectorElements.get(j).select(titleSelector).text();
-                    String oldPrice = (boxSelectorElements.get(j).select(oldPriceSelector).text()).substring(0, boxSelectorElements.get(j).select(oldPriceSelector).text().length() / 2);
-                    String currPrice = (boxSelectorElements.get(j).select(currPriceSelector).text()).substring(0, boxSelectorElements.get(j).select(currPriceSelector).text().length() / 4);
+                    String oldPrice = (boxSelectorElements.get(j).select(oldPriceSelector).text()).substring(0, boxSelectorElements.get(j).select(oldPriceSelector).text().indexOf(".") + 3);
+                    String currPrice = (boxSelectorElements.get(j).select(currPriceSelector).text()).substring(0, boxSelectorElements.get(j).select(currPriceSelector).text().indexOf(".") + 3);
                     String stars = boxSelectorElements.get(j).select(starSelector).text();
                     String[] newDataValues = {picture, title, oldPrice, currPrice, stars};
                     //                           0       1       2          3        4
-                    results += "{\"picture\": " + "\""+ picture + "\",\n";
-                    results += "\"title\": " + "\""+ title + "\",\n";
-                    results += "\"oldPrice\": " + "\""+ oldPrice + "\",\n";
-                    results += "\"currPrice\": " + "\""+ currPrice + "\",\n";
-                    results += "\"rating\": " + "\""+ stars + "\"},\n";
-                    if(!(currPrice.equals(""))){
+                    results += "{\"picture\": " + "\"" + picture + "\",\n";
+                    results += "\"title\": " + "\"" + title + "\",\n";
+                    results += "\"oldPrice\": " + "\"" + oldPrice + "\",\n";
+                    results += "\"currPrice\": " + "\"" + currPrice + "\",\n";
+                    results += "\"rating\": " + "\"" + stars + "\"},\n";
+                    if (!(currPrice.equals(""))) {
                         wasPrinted = true;
                     }
 
                     allDataValues.add(newDataValues);
 
+                    System.out.println(currPrice);
                 }
             }
             if(!(wasPrinted)){
                 cancelFactor++;
             }
-            if(cancelFactor>= 0 && (wasPrinted)){
+            if(cancelFactor>= 0 && (wasPrinted)) {
                 cancelFactor--;
             }
             i++;
---
+++ js/addedJS.js
@@ -0,0 +1,10 @@
+var formElems = document.getElementById("checkbox_form")
+for(int i = 0; i<5; i++){
+	var elem = formElems.elements[i]
+	if(elem.checked){
+		console.log("yee")
+		document.getElementById("Interests_form").innerHTML+= elem
+		
+	}
+}
+
